13c929e6c540026362fe1b22fa55982a0a8c16a0
Etienne1999 <geslin.etienne@sfr.fr>
2020-04-14 17:07:19 +0200

    Le filtre

--- achat.php
+++ achat.php
@@ -13,6 +13,7 @@
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
 	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="achat.css">
 
 </head>
 
@@ -23,48 +24,132 @@
 
 	<!-- Conteneur -->
 	<div class="container-fluid">
-		<div id="fond_accueil">
-
-			<!-- Tableau deroulant --> 
-
+		
 			<!-- Premier affichage -->
 			<h1> ECE EBAY </h1>
-
-			<div class="right_col_achat">
+		<div class="row">
+		  <div class="col-md-3">
+		  <div class="left_col_achat">
 				<h2> Filtrer </h2>
 				<div class="rechercheVendeur">
-					<form>
-						<p><input type="text" name="rechercheVendeur" placeholder="Chercher un vendeur "></p>
+					<form action="achat.php" method="post">
+						<p><input type="text" id="rechercheVendeur "name="rechercheVendeur" placeholder="Chercher un vendeur "></p>
 						<div >
-							<input type="submit" value="La loupe" />
+							<input type="submit" value="La loupe" id="button" />
 						</div>
 					</form>
 				</div>
-				<div class="check_les_catégories">
-					<form>
-						<p><label> Ferraille ou Trésor: </label><input type="checkbox" name="Check_Ferraille" ></p>
-						<p><label> Bon pour le musée : </label><input type="checkbox" name="Check_Musée" ></p>
-						<p><label> Accessoire VIP : </label><input type="checkbox" name="Check_VIP" ></p>
+				<div class="check_checkbox">
+					<form action="achat.php" method="post">
+						<p><label> Ferraille ou Trésor: </label><input type="checkbox" name="Check_Ferraille" id="Check_Ferraille"></p>
+						<p><label> Bon pour le musée : </label><input type="checkbox" name="Check_Musée" id="Check_Musée" ></p>
+						<p><label> Accessoire VIP : </label><input type="checkbox" name="Check_VIP" id="Check_VIP"></p>
+						<p><label> Enchere : </label><input type="checkbox" name="Check_enchere" id="Check_enchere"></p>
+						<p><label> Meilleure Offre : </label><input type="checkbox" name="Check_meilleur_offre" id="Check_meilleur_offre"></p>
+						<p><label> Achat immédiat : </label><input type="checkbox" name="Check_achat_im" id="Check_achat_im"></p>
+						<input type="submit" value="Valider les choix" id="button" />
 					</form>
 				</div>
-				<div class="check_les_types">
-					<form>
-						<p><label> Enchere : </label><input type="checkbox" name="Check_enchere" ></p>
-						<p><label> Meilleure Offre : </label><input type="checkbox" name="Check_meilleur_offre" ></p>
-						<p><label> Achat immédiat : </label><input type="checkbox" name="Check_achat_im" ></p>
-					</form>
-				</div>
-			</div>
-			<div class="achat_objet">
+		  </div>
+		  </div>
+
+
+
+
+
+		  <div class="col-md-9">
+			<!-- FILTRAGE -->
+			
+
+			<!-- Affichage selon le filtre -->
+			<div class="achat_objet_listening">
 				<?php
-					// fonction phph à coder --
+				$database = "ebay-ece (piscine)";
+				$db_handle = mysqli_connect('localhost', 'root', '' );
+				$db_found = mysqli_select_db($db_handle, $database);
+				
+				
+				$check_vendeur = isset($_POST["rechercheVendeur"])? $_POST["rechercheVendeur"] : "";
+				
+				if ($db_found) 
+					{	
+						// TESTER La recherche
+						if ($check_vendeur != '') {
+							$sql ="SELECT * FROM produit WHERE Vendeur =$check_vendeur";
+							$result = mysqli_query($db_handle, $sql);
+							if ($result != NULL) {				
+								while ($data = mysqli_fetch_assoc($result)) {
+									echo "ID: " . $data['ID'] . '<br>';
+									echo "Nom:" . $data['Nom'] .'<br>';
+									echo "prix: " . $data['Prix_Achat'] . '<br>';	
+								}							
+							}
+						}
+						
+						// Tester les checkbox
+						else{
+							$sql = "SELECT * FROM produit WHERE ID != 0 AND";
+							$test = 0;
+
+							//CHOIX FERRAILLE
+							if (isset($_POST["Check_Ferraille"])) 
+							{
+  								if ($test ==1)
+  								{
+  									$sql .= "OR ";
+  									$test = 0;
+  								}
+  								$sql .= " Categorie = 1 ";
+  								$test ++;
+  							}
+
+  							//CHOIX MUSEE
+							if (isset($_POST["Check_Musée"])) 
+							{
+  								if ($test ==1)
+  								{
+  									$sql .= "OR ";
+  									$test = 0;
+  								}
+  								$sql .= " Categorie = 2 ";
+  								$test ++;
+  							}
+
+  							//CHOIX VIP
+							if (isset($_POST["Check_VIP"])) 
+							{
+  								if ($test ==1)
+  								{
+  									$sql .= "OR ";
+  									$test = 0;
+  								}
+  								$sql .= " Categorie = 3 ";
+  								$test ++;
+  							}
+
+
+  							$result = mysqli_query($db_handle, $sql);
+  							if ($result != NULL) {	
+							while ($data = mysqli_fetch_assoc($result)) {
+								echo "ID: " . $data['ID'] . '<br>';
+								echo "Nom:" . $data['Nom'] .'<br>';
+								echo "prix: " . $data['Prix_Achat'] . '<br>';
+							}
+							}
+						}
+					}//end if
 
 				?>
 
 			</div>
+		  </div>
+		</div>	<!-- Tableau deroulant --> 
+
+
+
 
 			<br><br><br><br>
-		</div>
+		
 	</div>
 
 	<!-- Footer : contact -->	
