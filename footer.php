bf1e1c2b34e4d7ada2dc655b7898008d50042c81
Magic-System <taurloc@gmail.com>
2020-04-14 06:22:29 +0200

    Update footer.php
    Footer basique repris du tp7

--- footer.php
+++ footer.php
@@ -1,20 +1,26 @@
-<!-- Tableau deroulant --> 
 
-<div class="contact_us_footer">
-	<h2>Contact</h2>
-	<form method="post" action="#">
-		<div class="myRow">
-			<input type="text" name="myName" class="my" placeholder="My name"/>
+<footer class="page-footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-8 col-md-8 col-sm-12">
+				<h6 class="text-uppercase font-weight-bold">Informations additionnelle</h6>
+				<p>
+					Projet piscine ECE ing 3
+				</p>
+			</div>
+
+			<div class="col-lg-4 col-md-4 col-sm-12">
+				<h6 class="text-uppercase font-weight-bold">Contact</h6>
+				<p>
+					37, Quai de Grenelle, 75015 Paris, France <br>
+					info@webDynamique.ece.fr <br>
+					+33 01 02 03 04 05 <br>
+					+33 01 03 02 05 04
+				</p>
+			</div>
 		</div>
-		<div class="myRow">
-	
-	<br>				<input type="text" name="myEmail" class="my" placeholder="My Email"/>
-		</div>
-		<div >
-			<textarea name="myComments" class="my" rows="2" cols="20" placeholder="Comments :"></textarea>
-		</div>
-		<div class="mySubmit">
-			<input type="submit" value="Contact" />
-		</div>
-	</form>		
-</div>
+	</div>
+	<div class="footer-copyright text-center">
+		&copy; 2019 Copyright | Droit d'auteur : webDynamique.ece.fr
+	</div>
+</footer>
