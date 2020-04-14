2422b099423fee69086f8e1adbb9200905a94728
Magic-System <taurloc@gmail.com>
2020-04-14 06:18:32 +0200

    Correction template html
    - J'ai corriger les link de bootstrap. Ils etaient pas a jour je crois pcq y'avais des problèmes
    
    - J'ai refait la navbar plus propre
    
    - J'ai suppr accueil.php qui n'est pas utile, index.php est deja la page d'accueil

--- nav.php
+++ nav.php
@@ -1,55 +1,33 @@
 <!-- Tableau deroulant --> 
 
-<nav>
-
-	<ul>
-		<li class ="menu"> <a href="index.php"> ECE EBAY </a></li>
-
-
-		<li class ="menu"><a href="categorie.php">   Catégorie </a> 
-				<ul class ="sous-menu">
-					<li><a href="#">   Meilleure offre </a> </li>
-					<li><a href="#">   Enchère </a> </li>
-					<li><a href="#">   Catégorie </a> </li>
-					
-				</ul>
-		</li>
-
-
-		<li class ="menu"><a href="achat.php">   Achat </a> 
-				<ul class ="sous-menu">
-					<li><a href="#">   Meilleure offre </a> </li>
-					<li><a href="#">   Enchère </a> </li>
-					<li><a href="#">   Catégorie </a> </li>
-					
-				</ul>
-		</li>
-
-
-		<li class ="menu"><a href="vente.php"> Vente </a> 
-				<ul class ="sous-menu">
-					<li><a href="#">    </a> </li>
-					<li><a href="#">    </a> </li>
-					<li><a href="#">    </a> </li>
-				</ul>
-		</li>
-		
-
-		<li class ="menu"><a href="mon_compte.php"> Votre compte </a> 
-				<ul class ="sous-menu">
-					<li><a href="#">   1 </a> </li>
-					<li><a href="#">   2 </a> </li>
-
-				</ul>
-		</li>
-		
-
-		<li class ="menu"><a href="mon_panier.php"> Panier </a> 
-				<ul class ="sous-menu">
-					<li><a href="#">   3 </a> </li>
-					
-						
-				</ul>
-		</li>
-	</ul>
+<nav class="navbar navbar-light navbar-expand-md navigation-clean-button">
+    <div class="container">
+    	<a class="navbar-brand" href="#">Ebay ECE<br /></a>
+    	<button data-toggle="collapse" data-target="#navcol-1" class="navbar-toggler">
+    		<span class="navbar-toggler-icon"></span>
+    	</button>
+        <div class="collapse navbar-collapse show" id="navcol-1">
+            <ul class="nav navbar-nav mr-auto">
+                <li class="nav-item dropdown">
+                	<a data-toggle="dropdown" class="dropdown-toggle nav-link" href="categorie.php">Catégories </a>
+                    <div role="menu" class="dropdown-menu">
+                    	<a class="dropdown-item" href="#">Feraille ou Trésor</a>
+                    	<a class="dropdown-item" href="#">Bon pour le Musée</a>
+                    	<a class="dropdown-item" href="#">Accessoire VIP</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                	<a data-toggle="dropdown" class="dropdown-toggle nav-link" href="#achat.php">Achat</a>
+                    <div role="menu" class="dropdown-menu">
+                    	<a class="dropdown-item" href="#">Enchères</a>
+                    	<a class="dropdown-item" href="#">Achat immédiat</a>
+                    	<a class="dropdown-item" href="#">Meilleure offre</a>
+                    </div>
+                </li>
+                <li class="nav-item"><a class="nav-link" href="vente.php">Vente</a></li>
+                <li class="nav-item"><a class="nav-link" href="mon_panier.php">Panier</a></li>
+                <li class="nav-item"><a class="nav-link" href="mon_compte.php">Votre Compte</a></li>
+            </ul>
+        </div>
+    </div>
 </nav>
