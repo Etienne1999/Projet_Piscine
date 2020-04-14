ab179ffaf96120e624af9b681abf35c0dec3f7c1
Magic-System <taurloc@gmail.com>
2020-04-15 01:07:49 +0200

    Ajout contraintes + rename db
    -Rename db pour pas avoir d'espace dans le nom
    -Ajout des contraintes de suppression et de mise a jour sur toutes les clés étrangères

--- database/ebay-ece.sql
+++ database/ebay-ece.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  mar. 14 avr. 2020 à 12:11
+-- Généré le :  mer. 15 avr. 2020 à 01:06
 -- Version du serveur :  10.4.10-MariaDB
 -- Version de PHP :  7.3.12
 
@@ -19,10 +19,10 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `ebay-ece (piscine)`
+-- Base de données :  `ebay-ece`
 --
-CREATE DATABASE IF NOT EXISTS `ebay-ece (piscine)` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `ebay-ece (piscine)`;
+CREATE DATABASE IF NOT EXISTS `ebay-ece` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `ebay-ece`;
 
 -- --------------------------------------------------------
 
@@ -34,10 +34,11 @@ DROP TABLE IF EXISTS `adresse`;
 CREATE TABLE IF NOT EXISTS `adresse` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `Ligne_1` varchar(255) NOT NULL,
-  `Ligne_2` varchar(255) NOT NULL,
+  `Ligne_2` varchar(255) DEFAULT NULL,
   `Ville` varchar(255) NOT NULL,
   `Code_Postal` varchar(255) NOT NULL,
   `Pays` varchar(255) NOT NULL,
+  `Telephone` varchar(255) NOT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -57,8 +58,8 @@ CREATE TABLE IF NOT EXISTS `carte_bancaire` (
   `Adresse_Facturation` int(11) NOT NULL,
   PRIMARY KEY (`Numero_Carte`),
   KEY `Adresse_Facturation` (`Adresse_Facturation`),
-  KEY `Type` (`Type`),
-  KEY `ID_User` (`ID_User`)
+  KEY `carte_bancaire_ibfk_2` (`Type`),
+  KEY `carte_bancaire_ibfk_3` (`ID_User`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -107,13 +108,13 @@ DROP TABLE IF EXISTS `commande`;
 CREATE TABLE IF NOT EXISTS `commande` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `Acheteur` int(11) NOT NULL,
-  `Adresse_Livraison` int(11) NOT NULL,
+  `Adresse_Livraison` int(11) DEFAULT NULL,
   `Montant_total` double NOT NULL,
   `Date_Commande` datetime NOT NULL DEFAULT current_timestamp(),
   `Date_Livraison` date NOT NULL,
   PRIMARY KEY (`ID`),
-  KEY `Acheteur` (`Acheteur`),
-  KEY `Adresse_Livraison` (`Adresse_Livraison`)
+  KEY `commande_ibfk_1` (`Acheteur`),
+  KEY `commande_ibfk_2` (`Adresse_Livraison`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -129,7 +130,7 @@ CREATE TABLE IF NOT EXISTS `commande_detail` (
   `Quantité` int(11) NOT NULL,
   `Montant` double NOT NULL,
   PRIMARY KEY (`ID`),
-  KEY `Objet` (`Objet`)
+  KEY `commande_detail_ibfk_2` (`Objet`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -161,7 +162,7 @@ CREATE TABLE IF NOT EXISTS `enchere` (
   `Acheteur` int(11) NOT NULL,
   `Prix` double NOT NULL,
   PRIMARY KEY (`Objet`,`Acheteur`),
-  KEY `Acheteur` (`Acheteur`)
+  KEY `enchere_ibfk_1` (`Acheteur`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -176,7 +177,7 @@ CREATE TABLE IF NOT EXISTS `img_produit` (
   `Produit` int(11) NOT NULL,
   `URL` varchar(255) NOT NULL,
   PRIMARY KEY (`ID`),
-  KEY `Produit` (`Produit`)
+  KEY `img_produit_ibfk_1` (`Produit`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -195,8 +196,8 @@ CREATE TABLE IF NOT EXISTS `offre_achat` (
   `Statut` int(11) NOT NULL,
   `Tentative` int(11) NOT NULL,
   PRIMARY KEY (`ID`),
-  KEY `Acheteur` (`Acheteur`),
-  KEY `Produit` (`Produit`)
+  KEY `offre_achat_ibfk_1` (`Acheteur`),
+  KEY `offre_achat_ibfk_2` (`Produit`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -217,8 +218,8 @@ CREATE TABLE IF NOT EXISTS `produit` (
   `Vendeur` int(11) NOT NULL,
   `Categorie` int(11) NOT NULL,
   PRIMARY KEY (`ID`),
-  KEY `Categorie` (`Categorie`),
-  KEY `Vendeur` (`Vendeur`)
+  KEY `produit_ibfk_2` (`Vendeur`),
+  KEY `produit_ibfk_1` (`Categorie`)
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 
 --
@@ -226,9 +227,9 @@ CREATE TABLE IF NOT EXISTS `produit` (
 --
 
 INSERT INTO `produit` (`ID`, `Nom`, `Description`, `Video`, `Prix_Achat`, `Prix_Enchere`, `Date_fin_enchere`, `Vendeur`, `Categorie`) VALUES
-(10, 'Le précieux', 'test bijou', NULL, 500, NULL, NULL, 1, 3),
-(11, 'Pieces d\'or romain', 'Lot de 200 pieces', NULL, 2000, 1500, '2020-04-16 13:37:00', 2, 1),
-(12, 'Mona Lisa', 'tqt bro ', NULL, NULL, 1000000, '2020-04-19 23:55:00', 3, 2);
+(1, 'Le précieux', 'test bijou', NULL, 500, NULL, NULL, 1, 3),
+(2, 'Pieces d\'or romain', 'Lot de 200 pieces', NULL, 2000, 1500, '2020-04-16 13:37:00', 2, 1),
+(3, 'Mona Lisa', 'tqt bro ', NULL, NULL, 1000000, '2020-04-19 23:55:00', 3, 2);
 
 -- --------------------------------------------------------
 
@@ -295,10 +296,10 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
   `Password` varchar(255) NOT NULL,
   `Email` varchar(255) NOT NULL,
   `Adresse` int(11) DEFAULT NULL,
-  `Role` int(11) NOT NULL,
+  `Role` int(11) DEFAULT NULL,
   PRIMARY KEY (`ID`),
-  KEY `Adresse` (`Adresse`),
-  KEY `Role` (`Role`)
+  KEY `Role` (`Role`),
+  KEY `utilisateur_ibfk_1` (`Adresse`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 --
@@ -319,56 +320,56 @@ INSERT INTO `utilisateur` (`ID`, `Nom`, `Prenom`, `Pseudo`, `Password`, `Email`,
 --
 ALTER TABLE `carte_bancaire`
   ADD CONSTRAINT `carte_bancaire_ibfk_1` FOREIGN KEY (`Adresse_Facturation`) REFERENCES `adresse` (`ID`),
-  ADD CONSTRAINT `carte_bancaire_ibfk_2` FOREIGN KEY (`Type`) REFERENCES `type_carte` (`ID`),
-  ADD CONSTRAINT `carte_bancaire_ibfk_3` FOREIGN KEY (`ID_User`) REFERENCES `utilisateur` (`ID`);
+  ADD CONSTRAINT `carte_bancaire_ibfk_2` FOREIGN KEY (`Type`) REFERENCES `type_carte` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `carte_bancaire_ibfk_3` FOREIGN KEY (`ID_User`) REFERENCES `utilisateur` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `commande`
 --
 ALTER TABLE `commande`
-  ADD CONSTRAINT `commande_ibfk_1` FOREIGN KEY (`Acheteur`) REFERENCES `utilisateur` (`ID`),
-  ADD CONSTRAINT `commande_ibfk_2` FOREIGN KEY (`Adresse_Livraison`) REFERENCES `adresse` (`ID`);
+  ADD CONSTRAINT `commande_ibfk_1` FOREIGN KEY (`Acheteur`) REFERENCES `utilisateur` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `commande_ibfk_2` FOREIGN KEY (`Adresse_Livraison`) REFERENCES `adresse` (`ID`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `commande_detail`
 --
 ALTER TABLE `commande_detail`
-  ADD CONSTRAINT `commande_detail_ibfk_1` FOREIGN KEY (`ID`) REFERENCES `commande` (`ID`),
-  ADD CONSTRAINT `commande_detail_ibfk_2` FOREIGN KEY (`Objet`) REFERENCES `produit` (`ID`);
+  ADD CONSTRAINT `commande_detail_ibfk_1` FOREIGN KEY (`ID`) REFERENCES `commande` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `commande_detail_ibfk_2` FOREIGN KEY (`Objet`) REFERENCES `produit` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `enchere`
 --
 ALTER TABLE `enchere`
-  ADD CONSTRAINT `enchere_ibfk_1` FOREIGN KEY (`Acheteur`) REFERENCES `utilisateur` (`ID`),
-  ADD CONSTRAINT `enchere_ibfk_2` FOREIGN KEY (`Objet`) REFERENCES `produit` (`ID`);
+  ADD CONSTRAINT `enchere_ibfk_1` FOREIGN KEY (`Acheteur`) REFERENCES `utilisateur` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `enchere_ibfk_2` FOREIGN KEY (`Objet`) REFERENCES `produit` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `img_produit`
 --
 ALTER TABLE `img_produit`
-  ADD CONSTRAINT `img_produit_ibfk_1` FOREIGN KEY (`Produit`) REFERENCES `produit` (`ID`);
+  ADD CONSTRAINT `img_produit_ibfk_1` FOREIGN KEY (`Produit`) REFERENCES `produit` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `offre_achat`
 --
 ALTER TABLE `offre_achat`
-  ADD CONSTRAINT `offre_achat_ibfk_1` FOREIGN KEY (`Acheteur`) REFERENCES `utilisateur` (`ID`),
-  ADD CONSTRAINT `offre_achat_ibfk_2` FOREIGN KEY (`Produit`) REFERENCES `produit` (`ID`);
+  ADD CONSTRAINT `offre_achat_ibfk_1` FOREIGN KEY (`Acheteur`) REFERENCES `utilisateur` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `offre_achat_ibfk_2` FOREIGN KEY (`Produit`) REFERENCES `produit` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `produit`
 --
 ALTER TABLE `produit`
-  ADD CONSTRAINT `produit_ibfk_1` FOREIGN KEY (`Categorie`) REFERENCES `categorie` (`ID`),
-  ADD CONSTRAINT `produit_ibfk_2` FOREIGN KEY (`Vendeur`) REFERENCES `utilisateur` (`ID`);
+  ADD CONSTRAINT `produit_ibfk_1` FOREIGN KEY (`Categorie`) REFERENCES `categorie` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `produit_ibfk_2` FOREIGN KEY (`Vendeur`) REFERENCES `utilisateur` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
-  ADD CONSTRAINT `utilisateur_ibfk_1` FOREIGN KEY (`Adresse`) REFERENCES `adresse` (`ID`),
-  ADD CONSTRAINT `utilisateur_ibfk_2` FOREIGN KEY (`Role`) REFERENCES `role` (`ID`);
+  ADD CONSTRAINT `utilisateur_ibfk_1` FOREIGN KEY (`Adresse`) REFERENCES `adresse` (`ID`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `utilisateur_ibfk_2` FOREIGN KEY (`Role`) REFERENCES `role` (`ID`) ON DELETE SET NULL ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
