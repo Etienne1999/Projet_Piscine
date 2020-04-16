0121d44bea2fa696ad448746b27530c501418a0b
Magic-System <taurloc@gmail.com>
2020-04-16 06:06:30 +0200

    Ajout suppr + modif edition
    -Requete d'edition fini
    -Ajout modal suppression
    -Ajout requete suppression
    -Ajout de nb objet vendu sur la requete d'affichage

--- admin.php
+++ admin.php
@@ -13,11 +13,12 @@
 	//Recupere les utilisateurs
 	function recup_users($db_handle) {
 
-		$sql = "SELECT u.Nom, u.Prenom, u.Pseudo, u.Email, Role.Nom AS Role, COUNT(produit.ID) AS Nb_Vente
+		$sql = "SELECT u.ID, u.Nom, u.Prenom, u.Pseudo, u.Email, Role.Nom AS Role, COUNT(produit.ID) AS Nb_Vente, COUNT(commande_detail.Objet) AS Nb_Vendu
 		FROM utilisateur AS u
 		LEFT JOIN role ON u.Role = Role.ID
 		LEFT JOIN produit ON u.ID = produit.Vendeur
-		GROUP BY u.Pseudo";
+		LEFT JOIN commande_detail ON produit.ID = commande_detail.Objet
+		GROUP BY u.Pseudo, produit.ID";
 
 		$result = mysqli_query($db_handle, $sql);	
 
@@ -30,16 +31,22 @@
 			echo '<tr>';
 			echo '	<td>' . $data['Pseudo'] . '</td>';
 			echo '	<td>' . $data['Role'] . '</td>';
+			//Bouton edition + info de l'utilisateur de cette ligne
 			echo ' 	<td colspan="2"><button type="button" class="btn btn-success" data-toggle="modal" data-target="#Modal_edit"';
+			echo ' data-id="' . $data['ID'] . '"';
 			echo ' data-nom="' . $data['Nom'] . '"';
 			echo ' data-prenom="' . $data['Prenom'] . '"';
 			echo ' data-pseudo="' . $data['Pseudo'] . '"';
 			echo ' data-email="' . $data['Email'] . '"';
 			echo ' data-vente="' . $data['Nb_Vente'] . '"';
-			echo '">Edit</button>';
+			echo ' data-vendu="' . $data['Nb_Vendu'] . '"';
+			echo '">Editer</button>';
 
-
-			//echo ' 	<button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">Delete</button></td>';
+			//Button suppression + info de l'utilisateur de cette ligne
+			echo ' 	<button type="button" class="btn btn-danger" data-toggle="modal" data-target="#Modal_suppr"';
+			echo ' data-pseudo="' . $data['Pseudo'] . '"';
+			echo ' data-id="' . $data['ID'] . '"';
+			echo '>Supprimer</button></td>';
 			echo '</tr>';
 		}
 	}
@@ -54,8 +61,50 @@
 		while ($data = mysqli_fetch_assoc($result)) {
 			echo '<option value="' . $data['ID'] . '">' . $data['Nom'] . '</option><br>';
 		}
+
 	}
 
+	//Edition user
+	if (isset($_POST['btn_edit'])){
+
+		$id = isset($_POST["id"])? $_POST["id"] : "";
+		$nom = isset($_POST["nom"])? $_POST["nom"] : "";
+		$prenom = isset($_POST["prenom"])? $_POST["prenom"] : "";
+		$pseudo = isset($_POST["pseudo"])? $_POST["pseudo"] : "";
+		$email = isset($_POST["email"])? $_POST["email"] : "";
+		$role = isset($_POST["role"])? $_POST["role"] : "";
+
+		if (!(empty($id) || empty($nom) || empty($prenom) || empty($pseudo) || empty($email) || empty($role))) {
+			
+			$sql_update_user = "UPDATE utilisateur 
+						   SET Nom = '$nom',
+						   Prenom = '$prenom',
+						   Pseudo = '$pseudo',
+						   Email = '$email',
+						   Role = '$role'
+						   WHERE ID = '$id'";
+
+			$res = mysqli_query($db_handle, $sql_update_user);
+
+			//var_dump($res);
+		}
+	}
+
+	//Suppression user
+	if (isset($_POST['btn_suppr'])){
+
+		$id = isset($_POST["id"])? $_POST["id"] : "";
+		$pseudo = isset($_POST["pseudo"])? $_POST["pseudo"] : "";
+
+		if (!(empty($id) || empty($pseudo))) {
+			
+			$sql_delete_user = "DELETE FROM utilisateur WHERE ID = '$id'";
+
+			$res = mysqli_query($db_handle, $sql_delete_user);
+
+			//var_dump($res);
+		}
+	}
 ?>
 
 <!DOCTYPE html>
@@ -85,22 +134,36 @@
 			$('#Modal_edit').on('show.bs.modal', function (event) {
 				var button = $(event.relatedTarget)
 				//Recupere les données de l'utilisateur a editer
+				var id = button.data('id')
 				var nom = button.data('nom')
 				var prenom = button.data('prenom')
 				var pseudo = button.data('pseudo')
 				var email = button.data('email')
 				var vente = button.data('vente')
+				var vendu = button.data('vendu')
 				var modal = $(this)
 				//Rempli le modal avec les données approprié
 				modal.find('.modal-title').text('Utilisateur : ' + pseudo)
+				modal.find('.modal-body #id').val(id)
 				modal.find('.modal-body #nom').val(nom)
 				modal.find('.modal-body #prenom').val(prenom)
 				modal.find('.modal-body #pseudo').val(pseudo)
 				modal.find('.modal-body #email').val(email)
 				modal.find('.modal-body #vente').val(vente)
-				modal.find('.modal-body #vendu').val(vente)
+				modal.find('.modal-body #vendu').val(vendu)
 
+			});
 
+			$('#Modal_suppr').on('show.bs.modal', function (event) {
+				var button = $(event.relatedTarget)
+				//Recupere les données de l'utilisateur a editer
+				var id = button.data('id')
+				var pseudo = button.data('pseudo')
+				var modal = $(this)
+				//Rempli le modal avec les données approprié
+				modal.find('.modal-title').text('Utilisateur : ' + pseudo)
+				modal.find('.modal-body #id').val(id)
+				modal.find('.modal-body #pseudo').val(pseudo)
 			});
 
 		});
@@ -176,33 +239,35 @@
 		</div>
 	</div>
 
+<!-- Modal Edition utilisateur -->	
 	<div class="modal fade" id="Modal_edit" data-backdrop="static" tabindex="-1" role="dialog">
 		<div class="modal-dialog" role="document">
 			<div class="modal-content">
 				<div class="modal-header">
-					<h5 class="modal-title" id="exampleModalLabel">New message</h5>
+					<h5 class="modal-title" id="exampleModalLabel">Edition</h5>
 					<button type="button" class="close" data-dismiss="modal">
 						<span>&times;</span>
 					</button>
 				</div>
-				<div class="modal-body">
-					<form>
+				<form method="post">
+					<div class="modal-body">
 						<div class="form-group">
+							<input type="text" class="form-control" id="id" name="id" readonly hidden="true">
 							<label for="nom" class="col-form-label">Nom :</label>
-							<input type="text" class="form-control" id="nom" readonly>
+							<input type="text" class="form-control" id="nom" name="nom">
 
 							<label for="prenom" class="col-form-label">Prénom :</label>
-							<input type="text" class="form-control" id="prenom" readonly>
+							<input type="text" class="form-control" id="prenom" name="prenom">
 						</div>
 						<div class="form-group">
 							<label for="peusdo" class="col-form-label">Pseudo :</label>
-							<input type="text" class="form-control" id="pseudo">
+							<input type="text" class="form-control" id="pseudo" name="pseudo">
 
 							<label for="email" class="col-form-label">Email :</label>
-							<input type="text" class="form-control" id="email">
+							<input type="text" class="form-control" id="email" name="email">
 
 							<label for="role" class="col-form-label">Role :</label>
-							<select class="form-control" id="role">
+							<select class="form-control" id="role" name="role">
 								<?php 
 									set_role_options($db_handle);
 								 ?>
@@ -215,15 +280,46 @@
 							<label for="vendu" class="col-form-label">Produits vendu:</label>
 							<input type="text" class="form-control" id="vendu" readonly>
 						</div>
-					</form>
-				</div>
-				<div class="modal-footer">
-					<button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
-					<button type="button" class="btn btn-primary">Enregistrer</button>
-				</div>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+						<button type="submit" class="btn btn-primary" name="btn_edit">Enregistrer</button>
+					</div>
+				</form>
 			</div>
 		</div>
 	</div>
+<!-- Fin Modal Edition utilisateur -->	
+
+<!-- Modal suppression utilisateur -->	
+	<div class="modal fade" id="Modal_suppr" data-backdrop="static" tabindex="-1" role="dialog">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="exampleModalLabel">Suppression (Confirmation)</h5>
+					<button type="button" class="close" data-dismiss="modal">
+						<span>&times;</span>
+					</button>
+				</div>
+				<form method="post">
+					<div class="modal-body">
+						<div class="form-group">
+							<input type="text" class="form-control" id="id" name="id" readonly hidden="true">
+							<p>Voulez vous vraiment supprimer cet utilisateur ?</p>
+							<label for="peusdo" class="col-form-label">Pseudo :</label>
+							<input type="text" class="form-control" id="pseudo" name="pseudo" readonly>
+						</div>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-primary" data-dismiss="modal">Non</button>
+						<button type="submit" class="btn btn-danger" name="btn_suppr">Oui</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+<!-- Fin Modal suppression utilisateur -->	
+
 	<!-- Footer -->	
 	<?php include("footer.php") ?>
 </body>
