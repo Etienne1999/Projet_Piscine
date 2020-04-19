c4e017a5b8753dd00e839d8b67e18e29d0bf42cf
Magic-System <taurloc@gmail.com>
2020-04-19 09:53:06 +0200

    redirection
    redirection vers index si on vien pas d'une requete post

--- paiement.php
+++ paiement.php
@@ -1,141 +1,16 @@
-<?php
+<?php 
+
     if (session_status() == PHP_SESSION_NONE) {
         session_start();
     }
     include("database/db_connect.php");
+	
+	if (isset($_POST['payer'])) {
+		var_dump($_POST);
 
-    //Redirection vers login si pas d'utilisateur connecté
-    if (!isset($_SESSION['user_ID'])) 
-    {
-        header("Location: login.php");
-    }
+	}
+	else {
+		header("Location: index.php");
+	}
 
-    if (isset($_SESSION['panier']) && empty($_SESSION['panier'])) {
-        header("Location: index.php");
-    }
-
-?>
-
-<!DOCTYPE html>
-<html>
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Payment - Brand</title>
-    <link rel="stylesheet" href="css/paiement.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i">
-
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-</head>
-
-<body>
-
-    <!-- Navbar -->
-    <?php include("nav.php") ?>
-
-    <main class="page payment-page">
-        <section class="clean-block payment-form dark">
-            <div class="container">
-                <div class="block-heading">
-                    <h2 class="text-info">Paiement</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna, dignissim nec auctor in, mattis vitae leo.</p>
-                </div>
-                <form>
-                    <div class="products">
-                        <h3 class="title">Détail commande</h3>
-
-                        <?php
-                            $montant_total = 0;
-                            foreach ($_SESSION['panier'] as $item) {
-                                $sql = "SELECT Nom, Prix_Achat, Description FROM produit WHERE ID = '$item'" ;
-                                $res = mysqli_query($db_handle, $sql);
-                                //var_dump($res);
-                                while ($info_item = mysqli_fetch_assoc($res)) {
-                                    echo '<div class="item"><span class="price">' . $info_item['Prix_Achat'] . '€</span>
-                                        <p class="item-name">' . $info_item['Nom'] . '</p>
-                                        <p class="item-description">' . $info_item['Description'] . '</p>
-                                    </div>';
-                                    $montant_total += $info_item['Prix_Achat'];
-                                }
-                                
-                            }
-
-                            echo '<div class="total"><span>Total</span><span class="price">' . $montant_total . '€</span></div>';
-                        ?>
-                    </div>
-                    <div class="card-details">
-                        <h3 class="title">Credit Card Details</h3>
-                        <div class="form-row">
-                            <div class="col-sm-7">
-                                <div class="form-group"><label for="card-holder">Card Holder</label><input class="form-control" type="text" placeholder="Card Holder"></div>
-                            </div>
-                            <div class="col-sm-5">
-                                <div class="form-group"><label>Expiration date</label>
-                                    <div class="input-group expiration-date"><input class="form-control" type="text" placeholder="MM"><input class="form-control" type="text" placeholder="YY"></div>
-                                </div>
-                            </div>
-                            <div class="col-sm-8">
-                                <div class="form-group"><label for="card-number">Card Number</label><input class="form-control" type="text" id="card-number" placeholder="Card Number"></div>
-                            </div>
-                            <div class="col-sm-4">
-                                <div class="form-group"><label for="cvc">CVC</label><input class="form-control" type="text" id="cvc" placeholder="CVC"></div>
-                            </div>
-                            <div class="col-sm-12">
-                                <div class="form-group"><button class="btn btn-primary btn-block" type="submit">Proceed</button></div>
-                            </div>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </section>
-    </main>
-    <footer class="page-footer dark">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-3">
-                    <h5>Get started</h5>
-                    <ul>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Sign up</a></li>
-                        <li><a href="#">Downloads</a></li>
-                    </ul>
-                </div>
-                <div class="col-sm-3">
-                    <h5>About us</h5>
-                    <ul>
-                        <li><a href="#">Company Information</a></li>
-                        <li><a href="#">Contact us</a></li>
-                        <li><a href="#">Reviews</a></li>
-                    </ul>
-                </div>
-                <div class="col-sm-3">
-                    <h5>Support</h5>
-                    <ul>
-                        <li><a href="#">FAQ</a></li>
-                        <li><a href="#">Help desk</a></li>
-                        <li><a href="#">Forums</a></li>
-                    </ul>
-                </div>
-                <div class="col-sm-3">
-                    <h5>Legal</h5>
-                    <ul>
-                        <li><a href="#">Terms of Service</a></li>
-                        <li><a href="#">Terms of Use</a></li>
-                        <li><a href="#">Privacy Policy</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="footer-copyright">
-            <p>© 2018 Copyright Text</p>
-        </div>
-    </footer>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
-</body>
-
-</html>
+ ?>
